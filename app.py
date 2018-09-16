2e88514cc2a9e5df41bdbf7f1852b68cdcbe6869
Alex Alegre <alexander.alegre@outlook.com>
2018-09-16 15:16:58 -0700

    added postgress support

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +12,9 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db'
+)
 # TODO: try to get mysql to work
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
