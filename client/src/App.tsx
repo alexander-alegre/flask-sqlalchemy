3c4800ae059f9ba9d2e65b82ef8f13d92510bba9
Alex Alegre <alexander.alegre@outlook.com>
2018-09-28 23:18:49 -0700

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import * as React from 'react';
 
 interface IProps {
@@ -17,28 +16,12 @@ class App extends React.Component<IProps, IState> {
     };
   }
 
-  public componentDidMount() {
-    axios.get('http://localhost:5000/items', {
-      headers: {
-        'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MzcyNDYxMDEsImlhdCI6MTUzNzI0NTgwMSwibmJmIjoxNTM3MjQ1ODAxLCJpZGVudGl0eSI6MX0.RnG05g17_tn0pQIit30a9yqORjPPm6hPmuasnuPo0EM',
-      }
-    })
-      .then(res => {
-        window.console.log(res);
-        this.setState({ data: res.data.items });
-      })
-      .catch(err => {
-        window.console.log('err:', err);
-      })
-  };
 
   public render() {
     window.console.log('data:', this.state.data);
     return (
       <div className="App">
-        { this.state.data.map((key: any) => {
-          return key.name
-        })}
+        <p>Hello world!</p>
       </div>
     );
   }
